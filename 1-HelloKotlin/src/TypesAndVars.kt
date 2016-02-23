e65e4de5474d944b10aaa6f87867aaca2025ae51
liulixiang1988 <liulixiang1988@gmail.com>
2016-02-23 23:09:19 +0800

    Hello Kotlin

---
+++ 1-HelloKotlin/src/TypesAndVars.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    val_declarations()
+}
+
+fun val_declarations(){
+    val a:Int = 64
+    val b:Long = 123
+    val c:Float = 3.4f
+    val d:Double = 3.4
+    println("$a $b $c $d")
+}
