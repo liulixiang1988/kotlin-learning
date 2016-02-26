a2cfb8dbada7ea1e95ebbf7e0645a03bc1105082
liulixiang1988 <liulixiang1988@gmail.com>
2016-02-26 13:11:18 +0800

    变量声明

--- 1-HelloKotlin/src/TypesAndVars.kt
+++ 1-HelloKotlin/src/TypesAndVars.kt
@@ -1,14 +1,33 @@
 fun main(args: Array<String>) {
-    //val_declarations()
-    ranges()
+    val_declarations()
+    //ranges()
 }
 
 fun val_declarations(){
+    //val声明的变量不能更改
     val a:Int = 64
     val b:Long = 123
-    val c:Float = 3.4f
+
+    //这种事不行的,必须每个占一行:
+    //val c = 3, d = 4
+
+    val c:Float = 3.4f //后面跟'F'或者'f'
     val d:Double = 3.4
-    println("$a $b $c $d")
+    val e:Double = 12.3e5
+    //不赋值的val声明
+    val f: Int
+    f = 13
+
+    val g:StringBuffer = StringBuffer("test")
+    g.replace(0, 1, "T")
+    //打印
+    println("$a $b $c $d $e $f")
+    println("$g")
+
+    //可更改的变量
+    var h:Int = 123
+    h = 45
+    println("$h")
 }
 
 fun ranges(){
